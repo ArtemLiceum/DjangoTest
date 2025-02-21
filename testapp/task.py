c73bf073786c1ee67c0367bae569e39896efb10e
Artem <bedintema@gmail.com>
2025-02-21 12:29:48 +0300

    add file to send email

---
+++ testapp/task.py
@@ -0,0 +1,11 @@
+from background_task import background
+from django.core.mail import send_mail
+
+
+@background()
+def send_shop_update_email(shop_id, email):
+    subject = "Обновление магазина"
+    message = f"Магазин с ID {shop_id} был обновлён"
+    from_email = "bedintema@gmail.com"
+
+    send_mail(subject, message, from_email, [email])
